bede6968363173cc33e4424f5aa977e78e432713
karimkohel <kareemkohel@gmail.com>
2023-09-28 16:29:11 +0300

    training update

--- train/train.py
+++ train/train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 
-model = YOLO("yolov8m-cls.pt")
+model = YOLO("train/yolov8m-cls.pt")
 
 if __name__ == "__main__":
-    results = model.train(data="dataset/", epochs=30, amp=False, batch=2)
+    results = model.train(data="dataset/", epochs=10, amp=False, batch=2, plots=True)
