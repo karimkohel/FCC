12c8f0482c5d640ce90d4d37eb7d2e55761761e0
karimkohel <kareemkohel@gmail.com>
2023-09-29 11:48:13 +0300

    better typehinting

--- FCC/CarClassifier.py
+++ FCC/CarClassifier.py
@@ -2,6 +2,8 @@
 """
 from ultralytics import YOLO
 from ultralytics.engine.results import Results
+import numpy as np
+from typing import Union
 
 class CarClassifier():
     """CarClassifier is a wrapper class for the custom trained classification model to facilitate easier usage and code integration"""
@@ -21,16 +23,16 @@ class CarClassifier():
         self.model = YOLO(model)
         self.NAMES = self.model.names
 
-    def predict(self, imgSrc: str) -> str: # TODO: change the typehinting to str or ndarray
+    def predict(self, img: Union[str, np.ndarray]) -> str:
         """Use the class classifier model to make a prediction on one image and return the predicted car model name
         
         INPUT
         ---
-        imgSrc: required, the string path to the image or image object to predict it's class
+        img: required, the string path to the image or image object
 
         OUTPUT
         ---
         string containing the predicted class name
         """
-        results = self.model.predict(source=imgSrc)
+        results = self.model.predict(source=img)
         return self.NAMES[results[0].probs.top1]
