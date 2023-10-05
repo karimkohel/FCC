dddd08a10d75b15123fc7ff810c86e52fa9c2056
karimkohel <kareemkohel@gmail.com>
2023-10-05 13:05:06 +0300

    started the testing pipeline

---
+++ testing/cropper.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+import numpy as np
+import cv2
+
+def crop_cars(mode, frame: np.ndarray) -> None:
+    results = model.predict(source=frame, conf=0.1, stream=True)
+    for result in results:
+        for predictedClass in result.boxes.cls:
+            if int(predictedClass) in [2, 7]:
+                boxes = result.boxes.cpu().numpy()
+
+
+if __name__ == "__main__":
+    model = YOLO("testing/yolov8s.pt")
+    model.to('cuda')
+
+    frame = cv2.imread("testing/car.test.jpg")
+    crop_cars(model, frame)
